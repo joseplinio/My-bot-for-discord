c1932e6ac91b2681d6df0721c240ee418a53c623
José Plinio Ripardo Rocha <joseplinio2008@gmail.com>
2024-08-17 17:47:59 -0300

    mudando o global

--- utils/database.py
+++ utils/database.py
@@ -1,25 +1,26 @@
 # Importaçoes:
 import sqlite3
 
+# Banco de dodos global
+banco = sqlite3.connect('./data/database.db')
+
 # Funçao para um desafio da Sofia sobre database
-# Inicia o databese e usa dois parametros (id_user, count):
 def create_user():
     global banco
-    banco = sqlite3.connect(':memory:')
     cursor = banco.cursor()
     cursor.execute("create table logins (id_user INTEGER, chamadas INTEGER)")
     banco.commit()
 
 # Adiciona um usuario no banco de dados:
 def add_user(user_id, count):
-    banco = sqlite3.connect(':memory:')
+    global banco
     cursor = banco.cursor()
     cursor.execute("insert into logins values (?,?)", (user_id, count))
     banco.commit()
 
 # Pega o ID de um usuario no banco de dados:
 def get_user(user_id):
-    banco = sqlite3.connect(':memory:')
+    global banco
     cursor = banco.cursor()
     cursor.execute('select * from logins where id = ?',(user_id))
     user = cursor.fetchone([0])
