8134af05accdf37a4c66980217dc752bdcc6369e
José Plinio Ripardo Rocha <joseplinio2008@gmail.com>
2024-08-17 15:44:37 -0300

    add create_user, add_user, get_user

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-# Inportaçoes
+# Inportaçoes:
 from discord.ext import commands
 import discord
 import json
-from utils.database import init_db
-import os
 
-# Carregar configurações
+
+
+# Carregar configurações:
 with open('data/config.json') as config_file:
     config = json.load(config_file)
 
-# Configurar intents
+# Configurar intents:
 intents = discord.Intents.default()
 intents.message_content = True
         
-# Inicializar o bot
+# Inicializar o bot:
 bot = commands.Bot(command_prefix = config['prefix'], intents = intents)
 
-# Trazendo os comandos para a main ;]
+# Trazendo os comandos para a main ;] :
 async def load_extensions():
     initial_extensions = ['src.adventure','src.combat','src.inventory','src.count']
     for extension in initial_extensions:
         try:
             await bot.load_extension(extension)
-            print(f'\t\033[32mCog \033[m{extension}\033[32m carregada com sucesso\033[m!')
+            print(f'\t\033[32mSrc \033[m{extension}\033[32m carregada com sucesso\033[m!')
         except Exception as e:
             print(f'\033[31mErro ao carregar a cog {extension}: {e}\033[m') 
 
-# Mensagem de confirmação
+# Mensagem de confirmação:
 @bot.event
 async def on_ready():
     await load_extensions() # Carrega as Cogs
@@ -38,7 +38,7 @@ async def on_ready():
 
 
 # Inicia o banco de dados
-init_db()
+# Db is here.
 
 # Iniciar o bot
 bot.run(config['token'])
--- utils/database.py
+++ utils/database.py
@@ -1,32 +1,35 @@
 # Importaçoes:
 import sqlite3
-from discord.ext import commands
 
 # Funçao para um desafio da Sofia sobre database
 # Inicia o databese e usa dois parametros (id_user, count):
-
-def init_db(id_user, count):
+def create_user():
     global banco
     banco = sqlite3.connect(':memory:')
     cursor = banco.cursor()
-    cursor.execute("create table logins(id_user  INTEGER,chamadas INTEGER)")
-    cursor.execute("insert into logins values (?, ?)", (id_user, count))
+    cursor.execute("create table logins (id_user INTEGER, chamadas INTEGER)")
     banco.commit()
-    for c in cursor.execute("select * from logins"):
-        print(c)
-    banco.close()
 
-def check(ctx, m):
-    return m.author == ctx.author and m.channel == ctx.channel
+# Adiciona um usuario no banco de dados:
+def add_user(user_id, count):
+    banco = sqlite3.connect(':memory:')
+    cursor = banco.cursor()
+    cursor.execute("insert into logins values (?,?)", (user_id, count))
+    banco.commit()
 
+# Pega o ID de um usuario no banco de dados:
+def get_user(user_id):
+    banco = sqlite3.connect(':memory:')
+    cursor = banco.cursor()
+    cursor.execute('select * from logins where id = ?',(user_id))
+    user = cursor.fetchone([0])
+    return user
 
-async def create_user(ctx, id, count):
-    if check == check:
-        count += 1    
-    id_user = ctx.author.id
-    id = id_user
-    
+# Incrementa o contador do usuario aqui:
+def inc_user(id):
+    pass
 
-def get_user():
-
-    
+add_user(123,10)
+users = get_user()
+for user in users:
+    print(user)
