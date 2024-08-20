b90b4017be6173147999a3f298ce5ab680400ef1
José Plinio Ripardo Rocha <joseplinio2008@gmail.com>
2024-08-20 17:55:16 -0300

    add def cria_mostro ta faltando muita coisa

--- src/adventure.py
+++ src/adventure.py
@@ -4,12 +4,12 @@ from discord.ext import commands
 # Comandos para o bot:
 class Adventure(commands.Cog):
     def __init__(self, bot):
-         self.bot = bot
-     
-     # Comando explorar com a funçao de começar a exploraçao:
+        self.bot = bot
+
+    # Comando explorar com a funçao de começar a exploraçao:
     @commands.command(name='explorar')
     async def explore(self, ctx):
-         await ctx.send('Você está explorando uma área desconhecida...')
+        await ctx.send('Você está explorando uma área desconhecida...')
 
 # Define os comandos para o bot:
 async def setup(bot):
--- src/rpg.py
+++ src/rpg.py
@@ -1,8 +1,37 @@
 # Importaçao
 from discord.ext import commands
-from random import randint
+import json
+import discord
+
+# Carregar configurações:
+with open('data/config.json') as config_file:
+    config = json.load(config_file)
+
+intents = discord.Intents.default()
+intents.messages = True
+
+bot = commands.Bot(command_prefix=config['prefix'], intents=intents)
 
 # Cogs para o jogo
-class Init(commands.cog):
+class Rpg(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(name='criar_user')
+    async def cria_user(self, ctx):
+        pass
+    
+    def cria_monstro(self):
+        level = 2
+        monstro = {
+            "nome":"Mostro",
+            "lv": level,
+            "dano": level * 2,
+            "hp":level * 3.5
+        }
+        return monstro    
+    
+
+# Define os comandos para o bot:
+async def setup(bot):
+    await bot.add_cog(Rpg(bot)) 
