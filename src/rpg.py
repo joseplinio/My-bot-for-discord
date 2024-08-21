faa37e3865f4c2a1f349eb1bc935bfeb50be5fa0
José Plinio Ripardo Rocha <joseplinio2008@gmail.com>
2024-08-21 14:29:59 -0300

    add def create_user

--- src/rpg.py
+++ src/rpg.py
@@ -1,25 +1,39 @@
-# Importaçao
+# Importaçoes:
 from discord.ext import commands
-import json
-import discord
-
-# Carregar configurações:
-with open('data/config.json') as config_file:
-    config = json.load(config_file)
-
-intents = discord.Intents.default()
-intents.messages = True
-
-bot = commands.Bot(command_prefix=config['prefix'], intents=intents)
+import asyncio
 
 # Cogs para o jogo
 class Rpg(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='criar_user')
-    async def cria_user(self, ctx):
-        pass
+    @commands.command(name='criar_personagem')
+    async def cria_user(self, ctx, hp: float = 100, lv: int = 1 , exp: float = 0):
+        # Pergunta para o nome do personagem ao user:
+        await ctx.send('Qual vai ser o nome do seu personagem? : ')
+        
+        # Cheka quem mandou a mensagem e o canal que mandou
+        def check(m):
+            return m.author == ctx.author and m.channel == ctx.channel
+
+        try:
+            # Debug: Verificando se o bot está esperando pela resposta
+            response = await self.bot.wait_for("message",check=check,timeout=30.0)
+
+            # Responde o user:   
+            await ctx.send(f'Ótimo nome, **{response.content}!**')
+            
+            # Faz esperar a mensagem do bot de forma assíncrona:
+            await asyncio.sleep(1.5)
+
+            # Mostra os stats do user depois de criar o player:
+            await ctx.send(f"""```
+- |Seus Status|
+``` **Vida**: {hp}, **Lv**: {lv}, **Exp**: {exp}""")
+        # Debug: O tempo acabou:
+        except asyncio.TimeoutError:
+            await ctx.send('Voce demorou muito tempo para responder.')
+
     
     def cria_monstro(self):
         level = 2
