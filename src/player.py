78264ad7fff74682c0b10b328d482336156a3ef8
José Plinio Ripardo Rocha <joseplinio2008@gmail.com>
2024-08-26 20:38:16 -0300

    update validaçao de data

--- src/player.py
+++ src/player.py
@@ -22,9 +22,11 @@ class Player(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # Comando para criar um personagem:
     @commands.command(name='criar_personagem')
     async def create_character(self, ctx,):
         name = await self.pergunta_name(ctx)
+        # Se nao de o TimeOutErro o if e executado se nao o else:
         if name:
             user_data = {
                 "name": name,
@@ -33,6 +35,7 @@ class Player(commands.Cog):
                 "inventory": DEFAULT_INVENTORY,
                 "exp": DEFAULT_EXP,
             }
+            # Abre o ARQ.json e escreve nele o user_data:
             with open (f'{ctx.author.id}.json', 'w') as f:
                 json.dump(user_data, f)
             await  ctx.send(f'**Personagem *{name}* criado com sucesso!**') 
@@ -41,6 +44,10 @@ class Player(commands.Cog):
         
     
     async def pergunta_name(self, ctx):
+        # Lib pra validaçao de dota:
+        import re
+
+        # Pergunta o nome:
         await ctx.send('**Qual vai ser o nome do seu personagem? : **')
         
         def check(m):
@@ -48,10 +55,20 @@ class Player(commands.Cog):
         
         try:
             response = await self.bot.wait_for('message', check=check,timeout=30.0)
-            await ctx.send(f'**Ótimo nome,** *{response.content}* **!**')
-            await asyncio.sleep(1.3)
-            return response.content        
+            name = response.content.strip()  # Tira os espaços
+            name = re.sub(r'\s+', '_', name) # Substitui os espaços por __
+            sanitized_name = name.replace("<", "&lt;").replace(">", "&gt;") # Previnino contra ataque em HTML:
+
+            # Se o nome tiver caracteris malisiosos avisa:
+            if not re.match("^[A-Za-z0-9_-]*$", response.content):
+                await ctx.send("Nome inválido! Use apenas letras, números, hífens e sublinhados.")
+                return None
+            else:
+                await ctx.send(f'**Ótimo nome,** *{sanitized_name}* **!**')
+                await asyncio.sleep(1.3)
+                return sanitized_name
         except asyncio.TimeoutError:
+            # Error por esperar:
             await ctx.send('**Você demorou muito tempo para responder.**')
             return None
         
@@ -62,19 +79,25 @@ class Player(commands.Cog):
         def check(m):
             return m.author == ctx.author and m.channel == ctx.channel
         
+        # Falta validaçao de dos para a pergunta
+        
     @commands.command(name='view_stats')
     async def view_stats(self, ctx):
         try:
+            # Abre o ARQ.json so user e carega o user data:
             with open(f'{ctx.author.id}.json', 'r') as f:
                 user_data = json.load(f)
+            # Mostra os estatus do user:
             await ctx.send(f"**Status do Personagem:**\n"
                 f"Nome: **{user_data['name']}**\n"
                 f"Nível: **{user_data['level']}**\n"
                 f"Vida: **{user_data['life']}**\n"
                 f"Experiência: **{user_data['exp']}**")
         except FileNotFoundError:
+            # Se nao tiver o ARQ.json da esse error: 
             await ctx.send('Nenhum personagem encontrado. Por favor, crie um personagem primeiro usando o comando `!criar_personagem`.')
         
+
 # Define os comandos para o bot:
 async def setup(bot):
     await bot.add_cog(Player(bot))
