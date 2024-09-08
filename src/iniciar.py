b5fd797463037955794c2c5c61bf95023ef13441
José Plinio Ripardo Rocha <joseplinio2008@gmail.com>
2024-09-08 13:35:41 -0300

    mudanças nas cogs

--- src/iniciar.py
+++ src/iniciar.py
@@ -1,18 +1,18 @@
 # Importaçoes:
 from discord.ext import commands
-from src.player import Player
+from user import User
 
 # Class do comando count:
 class Count(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.player_cog = Player(bot)
+        self.user_cog = User(bot)
 
     # Comanmdo que mandar um mensage para o user sobre o inicio do RPG:
     @commands.command(name='iniciar_jornada')
     async def start_adventure(self, ctx):
         await ctx.send(f'`{ctx.author.name}` **iniciou uma aventura!**')
-        await self.player_cog.create_character(ctx)
+        await self.user_cog.create_character(ctx)
 
     @commands.command(name='tuturial')
     async def tuturial(self, ctx):
