b98e13f01655631ee950a73782eed9b265d5f135
José Plinio Ripardo Rocha <joseplinio2008@gmail.com>
2024-09-26 18:57:37 -0300

    add class inventario and item

---
+++ src/modls/inventario.py
@@ -0,0 +1,26 @@
+# Importaçoes:
+from item import Item
+
+# Class Inventario:
+class Inventario(Item):
+    
+    def __init__(self, nome: str, efeito: str, descricao: str, capacidade: 10) -> None:
+        super().__init__(nome, efeito, descricao,)
+        self.itens = []
+        self.capacidade = capacidade
+    
+    def add_item(self, item):
+        if self.itens > self.capacidade:
+            self.itens.append(item)
+            return True
+        return False
+    
+    
+    def remove_item(self, item):
+        if item in self.itens:
+            self.itens.remove(item)
+            return True
+        return False
+    
+    def list_item(self):
+        return [Item.nome for item in self.itens]
---
+++ src/modls/item.py
@@ -0,0 +1,7 @@
+# Class Item:
+class Item:
+    def __init__(self, nome: str, efeito:str, descricao: str) -> None:
+        self.nome = nome
+        self.efeito = efeito
+        self.descricao = descricao
+    
