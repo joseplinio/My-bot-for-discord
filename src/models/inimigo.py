3c63e2298ca55953643f5f3c46e2585b5f3f266e
José Plinio Ripardo Rocha <joseplinio2008@gmail.com>
2024-09-30 21:23:10 -0300

    mudei funçoes da classe inimigo e add outras...

--- src/models/inimigo.py
+++ src/models/inimigo.py
@@ -1,3 +1,6 @@
+# Importaçoes:
+import random
+
 # Classe Inimigo:
 class Inimigo:
     """
@@ -12,12 +15,14 @@ class Inimigo:
 
     def __init__(self, nome: str, vida: int, dano: int, exp: int, descricao: str = None):
         self._nome = nome
-        self._vida = max(vida, 0)  # Vida mínima é 0
-        self._dano = max(dano, 0)  # Dano mínimo é 0
-        self._exp = max(exp, 0)  # Experiência mínima é 0
+        self._vida_maxima = max(vida, 100) # Vida máxima mínima é 100
+        self._vida = self._vida_maxima
+        self._dano = max(dano, 15)  # Dano mínimo é 15
+        self._exp = max(exp, 20)  # Experiência mínima é 20
         self._player = None
-        self._descricao = descricao if descricao else self.gerar_descricao()
-
+        self._descricao = descricao if descricao else self._gerar_descricao()
+        self._recompensas = self._gerar_recompensas()
+        
     # Getters para acessar os atributos de forma controlada:
     @property
     def nome(self) -> str:
@@ -58,31 +63,52 @@ class Inimigo:
             raise ValueError('A experiência não pode ser negativa.')
         self._exp = novo_exp
 
-# Adiciona o jogador ao combate:
+    # Adiciona o jogador ao combate:
     def adiciona_jogador_ao_combate(self, player):
         from .player import Player
         if not isinstance(player, Player):
             raise ValueError('O objeto passado não é um Player.')
         self._player = player
 
-    def atacar_jogador(self) -> None:
+    # Ataca o jogador:
+    def atacar_jogador(self) -> tuple:
         """Inimigo ataca o jogador, causando dano à vida do jogador."""
         if self._player:
-            self._player.receber_dano(self._dano)
+            dano_causado = random.randint(self._dano + 2, self._dano - 2)
+            self._player.receber_dano(dano_causado)
+            return self._nome, dano_causado
         else:
             raise ValueError('Nenhum jogador está em combate com este inimigo.')
 
+    # Recebe o dano:
     def receber_dano(self, dano: int) -> None:
         """O inimigo recebe dano e reduz a quantidade de vida."""
         if dano < 0:
             raise ValueError('Dano não pode ser negativo.')
         self._vida = max(self._vida - dano, 0)  # Garante que a vida não fique negativa
 
-    def gerar_descricao(self) -> str:
+    # Gera um descriçao para o inimigo:
+    def _gerar_descricao(self) -> str:
         """Faz com o inimigo receba uma descrição aleatoria com ações e adjetivos para a descrição"""
-        # Iportaçao:
-        from random import choice
-        
         adjetivo = ["feroz", "assustador", "imponente", "misterioso", "ágil"]
         acoes =  ["espreita nas sombras", "olha fixamente para você", "prepara-se para atacar", "emite um som ameaçador"]
-        return f"Um {choice(adjetivo)} {self._nome} que {choice(acoes)}."
+        
+        return f"Um {random.choice(adjetivo)} {self._nome} que {random.choice(acoes)}."
+    
+    # Gera a recompensa do inimigo:
+    def _gerar_recompensas(self) -> list:
+        recompensas = []
+        if random.random < 0.5:
+            recompensas.append("Poção de Cura")
+        
+        if random.random < 0.3:
+            recompensas.append("Moeda de Ouro")
+        
+        if random.random < 0.1:
+            recompensas.append("Item Raro")
+        
+        return recompensas
+    
+    def morrer(self) -> tuple:
+        """Retorna a experiência e as recompensas quando o inimigo morre."""
+        return  self._exp, self._recompensas 
