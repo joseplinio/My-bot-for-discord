4623beec88c70676f226aebb11de4dd8364a0d6e
José Plinio Ripardo Rocha <joseplinio2008@gmail.com>
2024-10-29 15:43:34 -0300

    botes para a classe

---
+++ utils/interatividade/interface/botao_classes.py
@@ -0,0 +1,34 @@
+# Importaçoes:
+import discord
+from utils.interatividade.funcoes_for_bot.embed_utils import criar_embed
+
+class BotaoClasses(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+    @discord.ui.button(label="Guerreiro", style=discord.ButtonStyle.green)
+    async def botao_guerreiro(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if not interaction.response.is_done():
+            await interaction.response.defer()
+        await interaction.followup.send(await self.escolher_classe(interaction, "guerreiros"))
+    
+    @discord.ui.button(label="Alquimista", style=discord.ButtonStyle.green)
+    async def botao_alquimista(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if not interaction.response.is_done():
+            await interaction.response.defer()
+        await interaction.followup.send(await self.escolher_classe(interaction, "alquimista"))
+    
+    @discord.ui.button(label="Ranger", style=discord.ButtonStyle.green)
+    async def botao_ranger(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if not interaction.response.is_done():
+            await interaction.response.defer()
+        await interaction.followup.send(await self.escolher_classe(interaction, "ranger"))
+
+    async def escolher_classe(self, interaction: discord.Interaction, classe: str) -> None:
+        await interaction.followup.send(
+            embed=criar_embed(
+                descricao=f"Voce escolheu a classe **{classe.capitalize()}**",
+                color=discord.Color.dark_green()
+            ),
+            ephemeral=True
+        )
