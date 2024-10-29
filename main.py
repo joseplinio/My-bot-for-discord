5b00c6e7f38829a3ec1d8a45b44af264b00e9a63
José Plinio Ripardo Rocha <joseplinio2008@gmail.com>
2024-10-29 12:13:03 -0300

    Corrige importação do módulo msg_init

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 import os
 import asyncio
 import traceback
-from utils.interatividade.funcoes_for_bot.funcoes_bot import iniciar
+from utils.interatividade.funcoes_for_bot.msg_init import iniciar
 
 # Carega o .env:
 load_dotenv()
