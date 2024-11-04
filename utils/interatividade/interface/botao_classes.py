7742613ab87e9af6a48e6067343e47bdb8a299c5
José Plinio Ripardo Rocha <joseplinio2008@gmail.com>
2024-11-04 14:47:01 -0300

    fazendo a logica de criaçao do player com button

--- utils/interatividade/interface/botao_classes.py
+++ utils/interatividade/interface/botao_classes.py
@@ -2,24 +2,27 @@
 import discord
 from utils.interatividade.funcoes_for_bot.embed_utils import criar_embed
 from utils.interatividade.interface.botao_confirmacao import ConfirmacaoView
+from src.models.player import Player
 
 class BotaoClasses(discord.ui.View):
-    def __init__(self):
+    def __init__(self, nome):
         super().__init__()
-
+        self.nome = nome
+        
     @discord.ui.button(label="Guerreiro", style=discord.ButtonStyle.green)
     async def botao_guerreiro(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await self.escolher_classe(interaction, "Guerreiro")
+        await self.criar_personagem(interaction, "Guerreiro")
 
     @discord.ui.button(label="Alquimista", style=discord.ButtonStyle.green)
     async def botao_alquimista(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await self.escolher_classe(interaction, "Alquimista")
+        await self.criar_personagem(interaction, "Alquimista")
 
     @discord.ui.button(label="Ranger", style=discord.ButtonStyle.green)
     async def botao_ranger(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await self.escolher_classe(interaction, "Ranger")
+        await self.criar_personagem(interaction, "Ranger") 
 
-    async def escolher_classe(self, interaction: discord.Interaction, classe: str):
+    async def criar_personagem(self, interaction: discord.Interaction, classe: str):
+
         # Envia uma mensagem com a classe escolhida e pergunta se quer confirmar
         view_confirmacao = ConfirmacaoView(classe)
         await interaction.response.send_message(
@@ -41,6 +44,17 @@ class BotaoClasses(discord.ui.View):
                 ),
                 ephemeral=True
             )
+            # Parte de criar o player:
+            player = Player(self.nome, 1, 100, 20, {}, 0, classe)
+            
+            await interaction.followup.send(
+                embed=criar_embed(
+                    descricao=f"🎉 Parabéns! Seu personagem **{player.nome}** da classe **{player.classe}** foi criado!",
+                    color=discord.Color.green()
+                ),
+                ephemeral=True
+            )
+            
         else:
             await interaction.followup.send(
                 embed=criar_embed(
@@ -48,4 +62,5 @@ class BotaoClasses(discord.ui.View):
                     color=discord.Color.dark_orange()
                 ),
                 ephemeral=True,
-            )
+                view=BotaoClasses(self.nome)
+            )   
