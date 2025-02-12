e9ad9637c12f6f2d0fc2efe5512cd82106e04dc8
José Plínio Ripardo Rocha <joseplinio8002@gmail.com>
2025-02-12 15:53:15 -0300

    Somente mudando files e arquivos para outros diretorios e apagando arquivos desneces

--- src/cogs/class_types/alquimista.py
+++
@@ -1,8 +0,0 @@
-# Importaçoes
-
-class Alquimista():
-    def __init__(self):
-        pass
-
-    def ataque_class(self) -> int:
-        pass
--- src/cogs/class_types/ranger.py
+++
@@ -1,8 +0,0 @@
-# Importaçoes
-
-class Ranger():
-    def __init__(self):
-        pass
-
-    def ataque_class(self) -> int:
-        pass
